8096d299383c805d8c3607a359366fba8c51a312
Xuan Yan <xuanyan@xuanyan.ws>
2019-01-31 18:30:52 +0800

    improve code

--- app/router.js
+++ app/router.js
@@ -1,6 +1,7 @@
 module.exports = app => {
   const {router, controller, middleware} = app
-  const {address: addressMiddleware, pagination: paginationMiddleware} = middleware
+  const addressMiddleware = middleware.address()
+  const paginationMiddleware = middleware.pagination()
 
   router.get('/info', controller.info.index)
   router.get('/supply', controller.info.supply)
@@ -16,17 +17,65 @@ module.exports = app => {
   router.get('/raw-tx/:id', controller.transaction.rawTransaction)
   router.get('/recent-txs', controller.transaction.recent)
 
-  router.get('/address/:address', addressMiddleware(), controller.address.summary)
-  router.get('/address/:address/balance', addressMiddleware(), controller.address.balance)
-  router.get('/address/:address/balance/total-received', addressMiddleware(), controller.address.totalReceived)
-  router.get('/address/:address/balance/total-sent', addressMiddleware(), controller.address.totalSent)
-  router.get('/address/:address/balance/unconfirmed', addressMiddleware(), controller.address.unconfirmedBalance)
-  router.get('/address/:address/balance/staking', addressMiddleware(), controller.address.stakingBalance)
-  router.get('/address/:address/balance/mature', addressMiddleware(), controller.address.matureBalance)
-  router.get('/address/:address/txs', addressMiddleware(), paginationMiddleware(), controller.address.transactions)
-  router.get('/address/:address/utxo', addressMiddleware(), controller.address.utxo)
-  router.get('/address/:address/balance-history', addressMiddleware(), paginationMiddleware(), controller.address.balanceHistory)
-  router.get('/address/:address/qrc20-balance-history', addressMiddleware(), paginationMiddleware(), controller.address.qrc20BalanceHistory)
+  router.get(
+    '/address/:address',
+    addressMiddleware,
+    controller.address.summary
+  )
+  router.get(
+    '/address/:address/balance',
+    addressMiddleware,
+    controller.address.balance
+  )
+  router.get(
+    '/address/:address/balance/total-received',
+    addressMiddleware,
+    controller.address.totalReceived
+  )
+  router.get(
+    '/address/:address/balance/total-sent',
+    addressMiddleware,
+    controller.address.totalSent
+  )
+  router.get(
+    '/address/:address/balance/unconfirmed',
+    addressMiddleware,
+    controller.address.unconfirmedBalance
+  )
+  router.get(
+    '/address/:address/balance/staking',
+    addressMiddleware,
+    controller.address.stakingBalance
+  )
+  router.get(
+    '/address/:address/balance/mature',
+    addressMiddleware,
+    controller.address.matureBalance
+  )
+  router.get(
+    '/address/:address/txs',
+    addressMiddleware, paginationMiddleware,
+    controller.address.transactions
+  )
+  router.get(
+    '/address/:address/utxo',
+    addressMiddleware,
+    controller.address.utxo
+  )
+  router.get(
+    '/address/:address/balance-history',
+    addressMiddleware, paginationMiddleware,
+    controller.address.balanceHistory
+  )
+  router.get(
+    '/address/:address/qrc20-balance-history',
+    addressMiddleware, paginationMiddleware,
+    controller.address.qrc20BalanceHistory
+  )
 
-  router.get('/misc/rich-list', paginationMiddleware(), controller.misc.richList)
+  router.get(
+    '/misc/rich-list',
+    paginationMiddleware,
+    controller.misc.richList
+  )
 }
