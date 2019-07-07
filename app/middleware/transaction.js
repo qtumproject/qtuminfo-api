2b7083c02cb97dcc9aabff0d777f2c6be5733e43
Xuan Yan <xuanyan@xuanyan.ws>
2019-07-08 03:05:58 +0800

    fix typo

--- app/middleware/transaction.js
+++ app/middleware/transaction.js
@@ -1,7 +1,6 @@
 module.exports = () => async function transaction(ctx, next) {
   const db = ctx.model
   ctx.state.transaction = await db.transaction()
-  ctx.set('Access-Control-Allow-Origin', '*')
   try {
     await next()
     await ctx.state.transaction.commit()
